ec5ca90f96d8b6eb9d2c89e6fe55b657f47ffd3b
Smitpatel2000 <smitpatel28742@gmail.com>
2022-04-13 08:23:00 +0530

    cv2 added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
@@ -6,5 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/convertimg', methods=['POST'])
+def convertimg():
+    try:
+        if request.method == 'POST':
+            filename = request.files['image']
+            response = filename.read()
+            frame = cv2.imdecode(np.fromstring(response, np.uint8), cv2.IMREAD_COLOR)
+            detector = cv2.QRCodeDetector()
+            data, vertices_array, binary_qrcode = detector.detectAndDecode(frame) 
+            uuid=''            
+            if vertices_array is not None:
+                return data
+            else:
+                return "No data found"                
+    except Exception as e:
+        return ('error occured,' + str(e))
 if __name__ == "__main__":
     app.run()
